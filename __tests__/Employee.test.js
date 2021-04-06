49014564295e986967caa50f486bce2bfa67989c
Suejin Kim <kim.suejin.20@gmail.com>
2021-04-06 17:02:10 -0400

    edited files

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,12 @@
+const Employee = require ("./employee")
+const Manager = require ("./manager")
+const Engineer = require ("./engineer")
+const Intern = require ("./intern")
+
+describe("Employee class", () => {
+    it("Creates an object for Employee", () => {
+        const employee = new Employee("Name", "etc");
+        expect();
+        expect();
+    })
+})
