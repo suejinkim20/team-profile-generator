96b84e87f074af8d768459788df81b4221f262a7
Suejin Kim <kim.suejin.20@gmail.com>
2021-04-06 22:52:48 -0400

    wrote in manager questions for inquirer

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+const questions = require("./lib/questions");
 
+inquirer
+    .prompt(questions)
+    .then(answers => {
+        console.log(answers)
+    })
